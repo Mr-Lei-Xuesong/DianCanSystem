e847816188917c5f0cc5c26707ed24ebcdbb7f12
Mr-Lei-Xuesong <228389787@qq.com>
2020-06-16 15:55:47 +0800

    测试Login

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.demo.controller;
 
+import com.demo.entity.User;
+import com.demo.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +15,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Api(tags = "用户管理接口")
 @RestController()
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
 
+    @ApiOperation("用户登录的接口")
+    @ApiImplicitParam(name = "username",value = "用户名",defaultValue = "admin",required = true)
     @RequestMapping("/login")
-    public List<String >login(){
-           List<String > list = new ArrayList<>();
-           list.add("admin");
-           list.add("admin");
-           return list;
+    public User login(String username){
+        return userService.login(username);
     }
 }
