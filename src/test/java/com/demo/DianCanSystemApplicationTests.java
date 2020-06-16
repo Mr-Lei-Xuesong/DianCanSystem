9398de0c7cc50538b695b27cccda7a4b3ea5cce0
Mr-Lei-Xuesong <228389787@qq.com>
2020-06-16 15:43:34 +0800

    测试Login

--- src/test/java/com/demo/DianCanSystemApplicationTests.java
+++ src/test/java/com/demo/DianCanSystemApplicationTests.java
@@ -1,13 +1,20 @@
 package com.demo;
 
+import com.demo.entity.User;
+import com.demo.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DianCanSystemApplicationTests {
+    @Autowired
+    UserService userService;
 
     @Test
     void contextLoads() {
+        User login = userService.login("admin");
+        System.out.println(login);
     }
 
 }
