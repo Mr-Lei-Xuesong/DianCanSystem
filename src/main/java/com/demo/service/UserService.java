f58a92ef008c52e57de30567c2211b5fc811c102
Mr-Lei-Xuesong <228389787@qq.com>
2020-06-16 15:13:05 +0800

    添加Mapper

--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -1,7 +1,11 @@
 package com.demo.service;
 
+import com.demo.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    UserMapper userMapper;
 }
--- src/main/java/com/demo/service/WupinService.java
+++ src/main/java/com/demo/service/WupinService.java
@@ -1,7 +1,11 @@
 package com.demo.service;
 
+import com.demo.mapper.WupinMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WupinService {
+    @Autowired
+    WupinMapper wupinMapper;
 }
