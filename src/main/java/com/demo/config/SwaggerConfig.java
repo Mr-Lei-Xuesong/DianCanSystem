f720c56a7271eab11d676569a0d31f9032b6f31d
Mr-Lei-Xuesong <228389787@qq.com>
2020-06-17 09:37:49 +0800

    首页轮播图片

--- src/main/java/com/demo/config/SwaggerConfig.java
+++ src/main/java/com/demo/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,10 +17,20 @@ public class SwaggerConfig {
     @Bean
     public Docket createRestApi(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .pathMapping("/")
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.demo.controller"))
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("点餐系统")
+                .contact(new Contact("四组全体成员","",""))
+                .version("1.0")
+                .description("接口文档信息")
                 .build();
     }
 }
